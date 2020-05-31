2fa534c77f1c457a0db55ecc97f212fb757658cc
k4nchil-ID <66189256+k4nchil-ID@users.noreply.github.com>
2020-05-31 10:51:33 +0700

    Update and rename README.md to Jodoh.sh

---
+++ Jodoh.sh
@@ -0,0 +1,35 @@
+cari_jodoh.sh
+#!/bin/bash
+#gunakan tools ini😂
+#niscaya anda dapat jodoh
+echo "\033[0;34m]
+      .---.        .-----------
+     /     \  __  /    ------
+    / /     \(  )/    -----
+   //////   * \/ *   ---
+  //// / // :    : ---
+ // /   /  /*    *--
+//          //..\\
+===========UU====UU==================
++          *//||\\*                 +
++            ****                   +
++ coded by : ✿͜͡ঔৣ✪➵͜͡Kanchil-IDঔৣ͜᷍🌹      +
++ Team : Fsociety                   +
++ kontak me : 6282125068665         +
++                                   +
++  ^...^                 ^...^      +
++ <_* *_>   CARI JODOH  <_* *_>     +
++   \_/                   \_/       +
+====================================="
+
+echo "\033[0;32m]Siapa nama anda: "
+read anda
+echo "\033[0;32m]Masukan nama yg mau di jadikan jodoh: "
+read jodoh
+echo "\033[0;32m]Jumlah Mas kawin : "
+read kawin
+echo
+echo "Wait"
+echo
+echo "kami menikahkan $anda dengan $jodoh dengan mas kawin $kawin di bayar utang"
+echo "Selamat yaa"
